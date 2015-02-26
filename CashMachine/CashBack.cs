f66af21e8eda0d3482f135c562b431a94b466e70
Stelmashenko-A <stelma_28@bk.ru>
2015-02-26 16:54:46 +0300

    Создание базовых классов

---
+++ CashMachine/CashBack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class CashBack
+    {
+        public States Result { get; set; }
+        public Dictionary<int, int> Value { get; set; }
+    }
+}
---
+++ CashMachine/CashMachine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class CashMachine
+    {
+        Dictionary<int, int> Container { get; set; }
+        int NumberOfPars { get; set; }
+
+        public CashBack GetMoney(int Amount)
+        {
+            CashBack delivery = new CashBack();
+            return delivery;
+        }
+        States GetMoney(int Amount, CashBack cashBack, int RecursionLevel, int BankNotes)
+        {
+            
+            int CurrentParPosition = Container.Keys.Count()-1;
+            int CurrentPar = Container.Keys.ElementAt(CurrentParPosition);
+            int Remainder = Amount % CurrentPar;
+            if (BankNotes == 0 && Amount % CurrentPar != 0)
+            {
+                return States.AllCombinationsFailed;
+            }
+            if (Remainder == 0) return States.OK;
+            cashBack.Value[CurrentPar] = Amount / CurrentPar;
+            BankNotes += Amount / CurrentPar;
+            RecursionLevel++;
+            States result = States.CombinationFailed;
+            if (RecursionLevel < NumberOfPars)
+            {
+                result = States.NULL;
+                while (result != States.OK || result != States.AllCombinationsFailed);
+                {
+                    result = GetMoney(Remainder, cashBack, RecursionLevel, BankNotes);
+                    if(result == States.CombinationFailed)
+                    {
+                        if (cashBack.Value[CurrentPar] > 0)
+                        {
+                            cashBack.Value[CurrentPar]--;
+                            BankNotes--;
+                            Amount += CurrentPar;
+                        }
+                        else
+                        {
+                            return result;
+                        }
+                    }
+                } 
+            }
+            
+            return result;
+        }
+        
+    }
+}
